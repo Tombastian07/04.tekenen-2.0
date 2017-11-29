74eb990b8866c2b7f58c80f03d5cecc8c380b87d
Tom Bastian <tom.bastian204001@student.leeuwenborgh.nl>
2017-11-29 11:48:15 +0100

    ik heb een kunstwerk gemaakt

--- 04.tekenen 2.0/MainWindow.xaml.cs
+++ 04.tekenen 2.0/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace _04.tekenen_2._0
         {
             InitializeComponent();
         }
+
+        private void Circle_Click(object sender, RoutedEventArgs e)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Margin = new Thickness(0, 0, 150, 50);
+            ellipse.Height = 100;
+            ellipse.Width = 100;
+            ellipse.Fill = Brushes.DarkGoldenrod;
+            ellipse.Stroke = Brushes.Crimson;
+            ellipse.StrokeThickness = 7;
+            mainGrid.Children.Add(ellipse);
+        }
+        private void Circle2_Click(object sender, RoutedEventArgs e)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Margin = new Thickness(150, 0, 0, 50);
+            ellipse.Height = 100;
+            ellipse.Width = 100;
+            ellipse.Fill = Brushes.DarkGoldenrod;
+            ellipse.Stroke = Brushes.Crimson;
+            ellipse.StrokeThickness = 7;
+            mainGrid.Children.Add(ellipse);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle rec = new Rectangle();
+            rec.Fill = System.Windows.Media.Brushes.IndianRed;
+            rec.Stroke = System.Windows.Media.Brushes.GreenYellow;
+            rec.Height = 300;
+            rec.Width = 350;
+            mainGrid.Children.Add(rec);
+        }
+
+        private void Line1_Click(object sender, RoutedEventArgs e)
+        {
+            Line Line1 = new Line();
+            Line1.Stroke = Brushes.Black;
+            Line1.X1 = 150;
+            Line1.X2 = 250;
+            Line1.Y1 = 250;
+            Line1.Y2 = 250;
+            Line1.StrokeThickness = 7;
+            mainGrid.Children.Add(Line1);
+        }
     }
 }
